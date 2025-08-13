3269673818005091e4104c7da3acd100718e1581
TomParki97 <yescoachtom@gmail.com>
2025-08-13 12:23:11 -0400

    fix: prepare gh pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   base: '/mancalagame/',
-  plugins: [react()]
+  plugins: [
+    react(),
+    {
+      name: 'html-base-path',
+      enforce: 'pre',
+      transform(code, id) {
+        if (id.endsWith('index.html')) {
+          return code.replace(/\$\{import.meta.env.BASE_URL\}/g, '');
+        }
+      }
+    }
+  ]
 });
